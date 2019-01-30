8831c4a1e846a3e748406ec80c4aa6896c3ad370
roqqq3 <rooperasa@gmail.com>
2019-01-30 16:53:05 +0200

    added files

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "WoltAssignment"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.20.0"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.5"
---
+++ src/main/scala/MedianTimes.scala
@@ -0,0 +1,99 @@
+import com.github.nscala_time.time.Imports._
+import org.joda.time.format.DateTimeFormatter
+import scala.collection.mutable
+import scala.util.{Success, Try}
+import com.github.tototoshi.csv._
+import java.io._
+import scala.io.BufferedSource
+
+object MedianTimes extends App {
+  if (!validArguments(args)) {
+    println("Invalid arguments. See readme for more info if you're unsure.")
+  } else {
+    val day: Option[(LocalDateTime, LocalDateTime)] = parseDate(args(1), args(2))
+    val pickupData: Try[Map[String, Array[Array[String]]]] = readPickupData(args(0))
+    if (pickupData.isSuccess && day.isDefined) {
+      //parses the data and calculates the median pickup times for each location
+      val parsedData: Try[Array[(String, Int)]] = parsePickupData(pickupData.get, day.get)
+      //writes the data to a csv if parsing is successful
+      parsedData.map(writeData(_, args(0) + "/" + args(3))) match {
+        case _ @ Success(Success(_)) => println("Successfully calculated median times!")
+        case _ => println("Data writing failed. Pickup data may be corrupted.")
+      }
+    } else {
+      println("Data file missing or invalid time format given.")
+    }
+  }
+
+  private def writeData(data: Array[(String, Int)], fileName: String) =
+    Try {
+      val file: File = new File(s"data/$fileName")
+      CSVWriter.open(file)
+    }.map{ csvWriter =>
+      Try {
+        val formattedData: Seq[Seq[String]] = data.map(i => Seq(i._1, i._2.toString)).toSeq
+        val header: Seq[String] = Seq("location_id", "median_pickup_time")
+        csvWriter.writeAll(header +: formattedData)
+      }
+    }
+
+  /* Supported date formats:
+      DD-MM-YY
+      DD-MM-YYYY
+      YYYY-MM-DD */
+  private def parseDate(dateString: String, timeString: String) = {
+    val dateData: Array[String] = dateString.split("-")
+    val timeData: Array[String] = timeString.split("-")
+    //sets a pattern for parsing date based on the string
+    val format: Option[DateTimeFormatter] = dateData match {
+      case a if a(0).length == 4 && a(1).length == 2 && a(2).length == 2 => Some(DateTimeFormat.forPattern("YYYY-MM-DD-HH"))
+      case a if a(0).length == 2 && a(1).length == 2 && a(2).length == 4 => Some(DateTimeFormat.forPattern("DD-MM-YYYY-HH"))
+      case a if a(0).length == 2 && a(1).length == 2 && a(2).length == 2 => Some(DateTimeFormat.forPattern("DD-MM-YY-HH"))
+      case _ => None
+    }
+    //add leading zeroes to hours if needed
+    val timeStart: String = timeData.head.reverse.padTo(2, '0').reverse
+    val timeEnd: String = timeData.last.reverse.padTo(2, '0').reverse
+    format.map { i =>
+      val start: LocalDateTime = DateTime.parse(dateString + "-" + timeStart, i).toLocalDateTime
+      val end: LocalDateTime = DateTime.parse(dateString + "-" + timeEnd, i).toLocalDateTime
+      (start, end)
+    }
+  }
+
+  private def validArguments(args: mutable.WrappedArray[String]): Boolean = {
+    args.length == 4 &&
+      args(0).forall(_.isLetter) &&
+      (args(1).length == 8 || args(1).length == 10) &&
+      args(1).count(_ == '-') == 2 &&
+      args(1).split("-").forall(i => i.forall(_.isDigit)) &&
+      args(2).count(_ == '-') == 1 &&
+      args(2).split("-").forall(i => i.forall(_.isDigit)) &&
+      args(2).split("-").map(_.toInt).sliding(2).forall(i => i.head < i.last) &&
+      args(3).endsWith(".csv")
+  }
+
+  private def parsePickupData(pickupData: Map[String, Array[Array[String]]], date: (LocalDateTime, LocalDateTime)) = {
+    Try {
+      //filter to only include correct times
+      val filtered: Map[String, Array[Array[String]]] = pickupData.map{ i =>
+        (i._1, i._2.filter{ j =>
+          val iterDate: LocalDateTime = DateTime.parse(j(1)).toLocalDateTime
+          iterDate >= date._1 && iterDate <= date._2
+        })
+      }
+      //calculate the average pickup time
+      filtered.map{ i =>
+        (i._1, i._2.map(_.last.toInt).sum / i._2.length)
+      }.toArray.sortBy(_._1.toInt)
+    }
+  }
+
+  private def readPickupData(city: String): Try[Map[String, Array[Array[String]]]] = {
+    Try {
+      val pickupTimesFile: BufferedSource = io.Source.fromFile(s"data/$city/pickup_times.csv")
+      val pickupTimes: Array[Array[String]] = pickupTimesFile.getLines().drop(1).map(_.split(",")).toArray
+      pickupTimes.groupBy(_.head)
+    }
+  }
+}
